fe60fc09fd5d99a357b39e86118c9509a230c450
Ahmad-Ajm <ahaj82@hotmail.com>
2026-01-19 14:25:33 +0300

    feat(FEAT-001): add AppLayoutComponent with header/nav/footer

---
+++ code/src/app/layout/app-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app-layout.component.html',
+  styleUrl: './app-layout.component.css'
+})
+export class AppLayoutComponent {
+  year = new Date().getFullYear();
+}
